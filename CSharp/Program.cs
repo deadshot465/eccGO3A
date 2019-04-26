88061a00ce397695b5dacffeb2eaac989814644d
Chehui Chou <deadshot465@users.noreply.github.com>
2019-04-26 10:14:17 +0900

    Test commit.

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -6,7 +6,7 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Goodbye World!");
         }
     }
 }
