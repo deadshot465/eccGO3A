aa571f1e3753c25a97897b6a7feaa36fe1eb8306
Chehui Chou <deadshot465@users.noreply.github.com>
2019-05-01 04:25:25 +0900

    Kotlinバージョンを添加しました。

---
+++ Kotlin/src/K04.kt
@@ -0,0 +1,87 @@
+import java.lang.Exception
+import java.util.*
+
+class K04 : IExectuable() {
+
+    override fun question_01() {
+        try {
+            print("年齢を入力してください。＞")
+            val reader = Scanner(System.`in`)
+            val age = reader.nextInt()
+
+            if (age < 3 || age >= 70) {
+                println("入場料金無料です。")
+            } else {
+                println("通常料金です。")
+            }
+        } catch (ex: Exception) {
+            println(ex.message)
+        }
+    }
+
+    override fun question_02() {
+        try {
+            print("性別を選択してください。（０：男性　１：女性）＞")
+            val reader = Scanner(System.`in`)
+            val gender = reader.nextInt()
+
+            when (gender) {
+                0 -> println("あら、格好良いですね。")
+                1 -> println("あら、モデルさんみたいですね。")
+                else -> println("そんな選択肢はありません。")
+            }
+        }
+        catch (ex: Exception)
+        {
+            println(ex.message)
+        }
+    }
+
+    override fun question_03() {
+        try {
+            print("年齢を入力してください。＞")
+            val reader = Scanner(System.`in`)
+            val age = reader.nextInt()
+
+            if (age < 3 || age >= 70) {
+                println("入場料金無料です。")
+            } else if (age in 3..15) {
+                println("子供料金で半額です。")
+            } else if (age in 60..69) {
+                println("シニア割引で一割引きです。")
+            } else {
+                println("通常料金です。")
+            }
+
+        } catch (ex: Exception) {
+            println(ex.message)
+        }
+    }
+
+    override fun question_04() {
+        try {
+            println("＊＊＊おみくじプログラム＊＊＊")
+            print("おみくじを引きますか　（はい：１　いいえ：０）　＞")
+            val reader = Scanner(System.`in`)
+            val choice = reader.nextInt()
+
+            if (choice >= 1) {
+                val randomNumber = Random()
+                val oracle = randomNumber.nextInt(5)
+                when (oracle) {
+                    0 -> println("大吉　とってもいいことがありそう！！")
+                    1 -> println("中吉　きっといいことあるんじゃないかな")
+                    2 -> println("小吉　少しぐらいはいいことあるかもね")
+                    3 -> println("凶　今日はおとなしくておいた方がいいかも")
+                    4 -> println("大凶　これじゃやばくない？早く家に帰った方がいいかも")
+                    else -> {
+                    }
+                }
+            }
+        }
+        catch (ex: Exception)
+        {
+            println(ex.message)
+        }
+    }
+}
---
+++ Kotlin/src/K05.kt
@@ -0,0 +1,122 @@
+import java.lang.Exception
+import java.util.*
+
+class K05 : IExectuable() {
+
+    override fun question_01() {
+        var salary = 19.0f
+        var age = 22
+
+        while (salary < 50.0f) {
+            salary *= 1.035f
+            age++
+        }
+
+        print("${age}歳で月給${salary}万円")
+    }
+
+    override fun question_02() {
+        try {
+            var choice: Int
+
+            do {
+                println("起きろ～")
+                print("1．起きた　2．あと5分…　3．Zzzz…\t入力：")
+                val reader = Scanner(System.`in`)
+                choice = reader.nextInt()
+
+
+            } while (choice != 1)
+            println("よし、学校へ行こう！")
+        }
+        catch (ex: Exception)
+        {
+            println(ex.message)
+        }
+    }
+
+    override fun question_03() {
+        try {
+            var choice: Int
+
+            do {
+                println("起きろ～")
+                print("1．起きた　2．あと5分…　3．Zzzz…\t入力：")
+                val reader = Scanner(System.`in`)
+                choice = reader.nextInt()
+                if (choice == 1) {
+                    println("よし、学校へ行こう！")
+                }
+
+            } while (true)
+
+        } catch (ex: Exception) {
+            println(ex.message)
+        }
+    }
+
+    override fun question_04() {
+        try {
+            val randomNumber = Random()
+
+            val golem = Golem()
+            golem.hp = 300 + randomNumber.nextInt(200)
+            golem.defense = 80
+
+            var playerHp = 200 + randomNumber.nextInt(100)
+
+            println("ゴーレム　（HP:${golem.hp}　防御力：${golem.defense}）\n")
+
+            GolemLoop@ while (golem.hp > 0) {
+                System.out.println("残りHP：" + golem.hp)
+                print("攻撃手段を選択してください（1．攻撃　2．特技　3．魔法）　＞")
+                val reader = Scanner(System.`in`)
+                val option = reader.nextInt()
+                var damage: Int
+
+                when (option) {
+                    1 -> damage = 60 + randomNumber.nextInt(40)
+                    2 -> damage = 30 + randomNumber.nextInt(100)
+                    3 -> damage = 20 + randomNumber.nextInt(180)
+                    else -> continue@GolemLoop
+                }
+
+                println("基礎攻撃力は${damage}です。")
+
+                damage -= golem.defense
+
+                if (damage <= 0) {
+                    damage = 0
+
+                    println("ゴーレム：「ハハハハハ、情けないな！貴様は弱すぎる！」")
+                    println("ゴーレムがあなたを攻撃しました！攻撃値：${golem.attack}")
+                    playerHp -= golem.attack
+
+                    if (playerHp <= 0) {
+                        println("あなたはゴーレムに負けました！ゲームオーバー！")
+                        return
+                    }
+                    println("あなたの残りHPは：$playerHp")
+                }
+
+                println("ダメージは${damage}です")
+
+                golem.hp -= damage
+
+                if (golem.hp < 0) {
+                    golem.hp = 0
+                }
+
+                println("残りのHPは${golem.hp}です")
+
+                if (golem.hp == 0) {
+                    println("ゴーレムを倒しました！")
+                }
+            }
+        }
+        catch (ex: Exception)
+        {
+            println(ex.message)
+        }
+    }
+}
