b5693c27a77f4860770400b0a6f933afbf364699
Chehui Chou <deadshot465@users.noreply.github.com>
2019-05-07 04:01:05 +0900

    Rustの一部のエラー処理を改善しました。これからRustの全部のエラー処理を改善していきます。

--- rust/src/k02.rs
+++ rust/src/k02.rs
@@ -1,5 +1,6 @@
 use crate::golem::Golem;
 use crate::executable::Executable;
+use crate::read_user_input::read_user_input;
 use std::io::Write;
 
 pub struct K02 {}
@@ -27,15 +28,26 @@ impl Executable for K02
     fn question_2(&self)
     {
         print!("一つ目の整数は？");
-        std::io::stdout().flush();
+        std::io::stdout().flush().unwrap();
         let mut first_number = String::new();
-        std::io::stdin().read_line(&mut first_number).expect("入力失敗！");
-        let first_number = first_number.trim().parse::<i32>().expect("入力失敗！");
+        let first_number = match read_user_input(&mut first_number) {
+            Ok(x) => x,
+            Err(e) => {
+                println!("エラーが発生しました：{}", e.to_string());
+                return;
+            }
+        };
+
         print!("二つ目の整数は？");
-        std::io::stdout().flush();
+        std::io::stdout().flush().unwrap();
         let mut second_number = String::new();
-        std::io::stdin().read_line(&mut second_number).expect("入力失敗！");
-        let second_number = second_number.trim().parse::<i32>().expect("入力失敗！");
+        let second_number = match read_user_input(&mut second_number) {
+            Ok(x) => x,
+            Err(e) => {
+                println!("エラーが発生しました：{}", e.to_string());
+                return;
+            }
+        };
 
         println!("{}÷{}={}...{}", first_number, second_number, first_number / second_number, first_number % second_number);
     }
@@ -43,26 +55,48 @@ impl Executable for K02
     fn question_3(&self)
     {
         print!("一つ目の商品の値段は？");
-        std::io::stdout().flush();
+        std::io::stdout().flush().unwrap();
         let mut price_a = String::new();
-        std::io::stdin().read_line(&mut price_a).expect("入力失敗！");
-        let price_a = price_a.trim().parse::<i32>().expect("入力失敗！");
+        let price_a = match read_user_input(&mut price_a) {
+            Ok(x) => x,
+            Err(e) => {
+                println!("エラーが発生しました：{}", e.to_string());
+                return;
+            }
+        };
+
         print!("個数は？");
-        std::io::stdout().flush();
+        std::io::stdout().flush().unwrap();
         let mut amount_a = String::new();
-        std::io::stdin().read_line(&mut amount_a).expect("入力失敗！");
-        let amount_a = amount_a.trim().parse::<i32>().expect("入力失敗！");
+        let amount_a = match read_user_input(&mut amount_a) {
+            Ok(x) => x,
+            Err(e) => {
+                println!("エラーが発生しました：{}", e.to_string());
+                return;
+            }
+        };
 
         print!("二つ目の商品の値段は？");
-        std::io::stdout().flush();
+        std::io::stdout().flush().unwrap();
         let mut price_b = String::new();
-        std::io::stdin().read_line(&mut price_b).expect("入力失敗！");
-        let price_b = price_b.trim().parse::<i32>().expect("入力失敗！");
+        let price_b = match read_user_input(&mut price_b) {
+            Ok(x) => x,
+            Err(e) => {
+                println!("エラーが発生しました：{}", e.to_string());
+                return;
+            }
+        };
+
         print!("個数は？");
-        std::io::stdout().flush();
+        std::io::stdout().flush().unwrap();
         let mut amount_b = String::new();
-        std::io::stdin().read_line(&mut amount_b).expect("入力失敗！");
-        let amount_b = amount_b.trim().parse::<i32>().expect("入力失敗！");
+        let amount_b = match read_user_input(&mut amount_b) {
+            Ok(x) => x,
+            Err(e) => {
+                println!("エラーが発生しました：{}", e.to_string());
+                return;
+            }
+        };
 
         let total: f32 = (price_a * amount_a + price_b * amount_b) as f32 * 1.08;
         println!("お支払いは税込み￥{}です", total);
@@ -75,11 +109,17 @@ impl Executable for K02
         println!("ゴーレム　（HP:{}　防御力：{}）", golem.hp, golem.defense);
         println!("HP：{}", golem.hp);
         print!("今回の攻撃の値を入力してください＞");
-        std::io::stdout().flush();
+        std::io::stdout().flush().unwrap();
 
         let mut damage = String::new();
-        std::io::stdin().read_line(&mut damage).expect("入力失敗！");
-        let mut damage = damage.trim().parse::<i32>().expect("入力失敗！");
+        let mut damage = match read_user_input(&mut damage) {
+            Ok(x) => x,
+            Err(e) => {
+                println!("エラーが発生しました：{}", e.to_string());
+                return;
+            }
+        };
+
         damage =
             if damage - golem.defense > 0 {
                 damage - golem.defense
